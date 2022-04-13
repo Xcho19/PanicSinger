f7301cb39b64faa6d1433812c8e08354c5c8205f
Xcho <xach55@gmail.com>
2022-04-13 21:51:57 +0400

    Initial Commit

---
+++ PanicSinger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PanicSinger
+//
+//  Created by Xcho on 13.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
